f8307b19e6d8d1facbbcec4f2fb790c8bf33d657
Mohammed Bettaoui <bettapro100@gmail.com>
2020-03-03 15:13:00 +0100

    FirstUpdate

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var http = require('http');
 
-const hostname = '127.0.0.1';
 const port = 3000;
 var fs = require('fs');
 
   function onRequest(request, response){
   response.writeHead(200, {'Content-Type':'text/html'});
-  fs.readFile('./AppNode.html', null, function(error, data){
+  fs.readFile('./wavyCircle.html', null, function(error, data){
     if(error){
       response.writeHead(404);
       response.write('File not found!')
@@ -15,6 +14,7 @@ var fs = require('fs');
     }
     response.end();
   });
+
 
 }
 
